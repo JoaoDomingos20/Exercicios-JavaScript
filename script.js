63737ef1862fa5092856d4a3e1899a73f3f8948d
JoaoDomingos20 <cabdomingos@hotmail.com>
2023-03-31 18:57:44 +0100

    Realização do exercio 1 sobre tipos de variáveis

---
+++ script.js
@@ -0,0 +1,6 @@
+var Musica = 'Stairway to heaven'
+var Artist = 'Led Zeppelin'
+var Album = 4
+var isBestSong = true
+
+console.log('A música' + Musica + 'dos' + Artist + 'pertene ao' + Album + 'é a melhor de todos os tempos?' + isBestSong);
