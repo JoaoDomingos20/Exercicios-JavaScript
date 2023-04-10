36d82975e20e2e1f2d99d35cd793817a9d88df67
JoaoDomingos20 <128627282+JoaoDomingos20@users.noreply.github.com>
2023-04-10 14:32:38 +0100

    Add files via upload

---
+++ Reverse string/script.js
@@ -0,0 +1,10 @@
+function reverter() {
+
+    var nome = document.getElementById('input').value;
+
+    document.getElementById('result').innerHTML = nome.split('').reverse().join('');
+    
+
+
+    
+}
